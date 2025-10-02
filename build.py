74112a3eeb07cfd7019412ac585416d94e2ee09a
notkirb <itnojheals@gmail.com>
2025-10-01 21:34:49 -0600

    try to fix github actions tkinter issues

--- build.py
+++ build.py
@@ -1,6 +1,42 @@
 import subprocess
 import os
+import sys
+import tkinter
 
+# Find tkinter files to bundle in binary
+def get_tcl_tk_data():
+    paths = []
+
+    # Tcl and Tk library are in different places on some distros
+    possible_tcl = [
+        os.path.join(sys.prefix, "tcl"),
+        os.path.join(sys.prefix, "lib", "tcl"),
+        "/usr/share/tcltk/tcl8.6",
+        "/usr/lib/tcl8.6",
+        "/usr/share/tcltk",
+    ]
+    possible_tk = [
+        os.path.join(sys.prefix, "tk"),
+        os.path.join(sys.prefix, "lib", "tk"),
+        "/usr/share/tcltk/tk8.6",
+        "/usr/lib/tk8.6",
+        "/usr/share/tk8.6",
+    ]
+
+    for path in possible_tcl:
+        if os.path.exists(path):
+            paths.append(f"{path}{os.pathsep}tcl")
+            break
+
+    for path in possible_tk:
+        if os.path.exists(path):
+            paths.append(f"{path}{os.pathsep}tk")
+            break
+
+    return paths
+
+
+# Imports that aren't explicitly listed in requirements.txt
 hidden_imports = [
     "PIL._tkinter_finder",
     "tkinter",
@@ -28,10 +64,6 @@ cmd = [
     "main.py"
 ]
 
-for pkg in hidden_imports:
-    if f"--hidden-import={pkg}" not in cmd:
-                cmd.append(f"--hidden-import={pkg}")
-
 # Read requirements.txt if it exists
 if os.path.exists("requirements.txt"):
     with open("requirements.txt") as f:
@@ -43,6 +75,15 @@ if os.path.exists("requirements.txt"):
             # Avoid duplicates
             if f"--hidden-import={pkg}" not in cmd:
                 cmd.append(f"--hidden-import={pkg}")
+
+# Append hidden_imports to cmd
+for pkg in hidden_imports:
+    if f"--hidden-import={pkg}" not in cmd:
+                cmd.append(f"--hidden-import={pkg}")
+
+# append tkinter files
+for data in get_tcl_tk_data():
+    cmd.append(f"--add-data={data}")
 
 print("Running:", " ".join(cmd))
 subprocess.run(cmd)
